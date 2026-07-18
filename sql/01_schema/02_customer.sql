d94c8171d5925ebff950f6090b3f4e4d484919c6
Kajal <kajal.kumari@sbu.ac.in>
2026-07-19 00:13:06 +0530

    Refine customers PK constraint definition
    Updates the `customers` table DDL to declare `customer_id` as `NOT NULL` and define the primary key using a named table-level constraint (`pk_customers`). This makes constraint naming explicit and improves schema clarity and maintainability.

--- sql/01_schema/02_customer.sql
+++ sql/01_schema/02_customer.sql
@@ -6,12 +6,16 @@
  Dependencies : None
  ===============================================================================
  */
-CREATE TABLE customers (
-    customer_id VARCHAR(50) PRIMARY KEY,
+CREATE TABLE customers
+(
+    customer_id VARCHAR(50) NOT NULL,
     customer_unique_id VARCHAR(50) NOT NULL,
     customer_zip_code_prefix INTEGER NOT NULL,
     customer_city VARCHAR(100) NOT NULL,
-    customer_state CHAR(2) NOT NULL
+    customer_state CHAR(2) NOT NULL,
+
+    CONSTRAINT pk_customers
+     PRIMARY KEY (customer_id)
 );
 -- Constraints
 -- Notes
