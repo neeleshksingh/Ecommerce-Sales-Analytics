da7fc5888e3f2bfc9928019a43c1408b37dd0841
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2026-07-21 20:37:16 +0530

    Update 03_check_constraints.sql

---
+++ sql/02_constraints/03_check_constraints.sql
@@ -0,0 +1,7 @@
+/*
+===============================================================================
+File        : 03_check_constraints.sql
+Description : Adds CHECK constraints to the Ecommerce Sales Analytics database.
+Dependencies: All tables in 01_schema must exist before executing this script.
+===============================================================================
+*/
