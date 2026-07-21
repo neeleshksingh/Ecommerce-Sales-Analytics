e823e3fdda5ea6c3827f1b17cc64a3fac712c04b
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2026-07-21 20:31:32 +0530

    Update 01_foreign_keys.sql

--- sql/02_constraints/01_foreign_keys.sql
+++ sql/02_constraints/01_foreign_keys.sql
@@ -6,19 +6,15 @@ Dependencies: All tables in 01_schema must exist before executing this script.
 ===============================================================================
 */
 
-
 ALTER TABLE orders
 ADD CONSTRAINT fk_orders_customers
 FOREIGN KEY (customer_id)
 REFERENCES customers(customer_id);
 
-
-
 ALTER TABLE products
 ADD CONSTRAINT fk_products_product_category_translation
 FOREIGN KEY (product_category_name)
 REFERENCES product_category_translation(product_category_name);
-
 
 ALTER TABLE order_reviews
 ADD CONSTRAINT fk_order_reviews_orders
@@ -27,7 +23,7 @@ REFERENCES orders(order_id);
 
 ALTER TABLE order_payments
 ADD CONSTRAINT fk_order_payments_orders
-FOREIGN KEY  (order_id)
+FOREIGN KEY (order_id)
 REFERENCES orders(order_id);
 
 ALTER TABLE order_items
