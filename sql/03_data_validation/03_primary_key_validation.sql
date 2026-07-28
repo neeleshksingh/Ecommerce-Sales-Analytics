eff6f9e2969bc2fe71771514946ea337f4d105e8
Neelesh Kumar Singh <108358262+neeleshksingh@users.noreply.github.com>
2026-07-28 20:17:22 +0530

    Refactor primary key validation SQL script for improved readability and consistency

--- sql/03_data_validation/03_primary_key_validation.sql
+++ sql/03_data_validation/03_primary_key_validation.sql
@@ -1,24 +1,65 @@
+-- Customer Table Primary Key Validation
 SELECT
-
-customer_id,
-
-COUNT(*)
-
+    customer_id,
+    COUNT(*) AS duplicate_count
 FROM customers
-
 GROUP BY customer_id
+HAVING COUNT(*) > 1;
 
-HAVING COUNT(*)>1;
+-- Order Table Primary Key Validation
+SELECT
+    order_id,
+    COUNT(*)
+FROM orders
+GROUP BY order_id
+HAVING COUNT(*) > 1;
 
+-- Product Table Primary Key Validation
+SELECT
+    product_id,
+    COUNT(*)
+FROM products
+GROUP BY product_id
+HAVING COUNT(*) > 1;
+
+-- Seller Table Primary Key Validation
+SELECT
+    seller_id,
+    COUNT(*)
+FROM sellers
+GROUP BY seller_id
+HAVING COUNT(*) > 1;
+
+-- Order Items Table Primary Key Validation
+SELECT
+    order_id,
+    order_item_id,
+    COUNT(*)
+FROM order_items
+GROUP BY
+    order_id,
+    order_item_id
+HAVING COUNT(*) > 1;
+
+-- Order Payments Table Primary Key Validation
+SELECT
+    order_id,
+    payment_sequential,
+    COUNT(*)
+FROM order_payments
+GROUP BY
+    order_id,
+    payment_sequential
+HAVING COUNT(*) > 1;
+
+--NULL Validation
+SELECT
+    COUNT(*) AS null_customer_ids
+FROM customers
+WHERE customer_id IS NULL;
 
 SELECT
-
-review_id, order_id,
-
-COUNT(*)
-
-FROM order_reviews
-
-GROUP BY review_id, order_id
-
-HAVING COUNT(*)>1;
+    COUNT(*) AS invalid_rows
+FROM order_items
+WHERE order_id IS NULL
+   OR order_item_id IS NULL;
